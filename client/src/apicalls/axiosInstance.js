29b2d2aae9a69a67b330d8cde1b84bedc9893723
Sumanth-Vallabhaneni-03 <165824292+Sumanth-Vallabhaneni-03@users.noreply.github.com>
2024-10-19 16:38:31 +0530

    fix: cors

--- client/src/apicalls/axiosInstance.js
+++ client/src/apicalls/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    headers : {
-         authorization : `Bearer ${localStorage.getItem('token')}`
+    baseURL: 'https://edulib-backend.vercel.app', // Set the base URL for the API
+    headers: {
+        authorization: `Bearer ${localStorage.getItem('token')}`
     },
-});// 
+});
